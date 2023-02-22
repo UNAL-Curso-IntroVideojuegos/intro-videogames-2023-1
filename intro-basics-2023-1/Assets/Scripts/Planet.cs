e8a13f29f2c9eeeb61ede0f3d7af636fa1d175d3
Alejo Alvarez <juaalvarezme@gmail.com>
2023-02-21 20:21:29 -0500

    Added Trails to Planet and Space-ship

--- intro-basics-2023-1/Assets/Scripts/Planet.cs
+++ intro-basics-2023-1/Assets/Scripts/Planet.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace IntroGames.Planets
@@ -6,14 +7,26 @@ namespace IntroGames.Planets
     public class Planet : MonoBehaviour
     {
 
-        [Header("Movement")] [SerializeField] private float _movementSpeed = 30f; // degrees/sec
+        [Header("Movement")] 
+        [SerializeField] private float _movementSpeed = 30f; // degrees/sec
         [SerializeField] private float _radius = 20f;
 
-        [Header("Rotation")] [SerializeField] private float _localRotationSpeed; // degrees/sec
+        [Header("Rotation")]
+        [SerializeField] private float _localRotationSpeed; // degrees/sec
 
-        [Space(20)] [SerializeField] private Transform _center;
+        [Space(20)]
+        [SerializeField] private Transform _center;
 
         private float _angleMovementRad; //rad
+        
+        //Trail VFX
+        private TrailRenderer _trailRenderer;
+        private float _trailDelay = 0.1f; // delay to enable the trail
+
+        private void Start()
+        {
+            _trailRenderer = GetComponentInChildren<TrailRenderer>();
+        }
 
         void Update()
         {
@@ -23,9 +36,12 @@ namespace IntroGames.Planets
             //RotateAround();
             CircleMovement();
 
-            //Rotation
+            //Own Rotation
             //IndependentRotation();
             LookAtCenter();
+
+            //Trail VFX
+            UpdateTrailTime();
         }
 
         void RotateAround()
@@ -52,6 +68,23 @@ namespace IntroGames.Planets
             Vector3 rot = transform.eulerAngles;
             rot.z = lookAngle;
             transform.eulerAngles = rot;
+        }
+        
+        void UpdateTrailTime()
+        {
+            //If null -> return
+            if(!_trailRenderer)
+                return;
+            
+            if (_trailDelay > 0f)
+            {
+                _trailDelay -= Time.deltaTime;
+                _trailRenderer.time = 0;
+            }
+            else
+            {
+                _trailRenderer.time = 360f / _movementSpeed; //T = X/V
+            }
         }
     }
 }
