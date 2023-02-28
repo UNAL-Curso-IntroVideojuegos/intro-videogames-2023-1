0c00743f4c9ab49cb30a0aed8d417fce3b2810e3
Activeone/mateo.alvarez <mateo.alvarez@activeone.co>
2023-02-27 23:34:46 -0500

    Added enemy tank
    Added enemy tank with movement between 2 points and barrels that follow the player's tank.

---
+++ intro-basics-2023-1/Assets/Scripts/TankEnemy.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankEnemy : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform _playerTank;
+
+    [SerializeField]
+    private Transform[] _cannons;
+    [SerializeField]
+    private Transform _startPoint;
+    [SerializeField]
+    private Transform _endPoint;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {   
+        Vector3 start = _startPoint.position;
+        Vector3 end = _endPoint.position;
+       
+        // Divide to slow down the movement
+        float alternatingValue = Mathf.PingPong(Time.time / 2, 1);
+
+        Vector3 newPosition = Vector3.Lerp(start, end, alternatingValue);
+        transform.position = newPosition;
+
+        // Rotate cannons
+        foreach (Transform cannon in _cannons) 
+        {
+          //Cannon Rotation
+          Vector3 aimVector = (_playerTank.position - transform.position).normalized;
+          float angle = Mathf.Atan2(aimVector.y, aimVector.x) * Mathf.Rad2Deg + 90;
+          cannon.rotation = Quaternion.Euler(0,0,angle);
+        }
+    }
+
+}
