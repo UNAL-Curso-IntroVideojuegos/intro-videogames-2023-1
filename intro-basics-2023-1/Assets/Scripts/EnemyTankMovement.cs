49b9a4fad697a78ec81c2bfe5b07c4bf02863275
Davidcito12 <dlopezza@unal.edu.co>
2023-03-02 01:55:47 -0500

    Added enemy tank movement

---
+++ intro-basics-2023-1/Assets/Scripts/EnemyTankMovement.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTankMovement : MonoBehaviour
+{
+    [SerializeField] private Transform _turret1;
+    [SerializeField] private Transform _turret2;
+    [SerializeField] private Transform _startMovPoint;
+    [SerializeField] private Transform _endMovPoint;
+    private int current = 0;
+    private float speed = 1;
+    private Rigidbody2D _rb;
+    private float _rotAngle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (current == 0)
+        {
+            float distancia = Vector3.Distance(transform.position, _endMovPoint.position);
+            if (distancia < 1f)
+            {
+                current = 1;
+            }
+            Vector3 direccion = (transform.position - _endMovPoint.position).normalized;
+            float velocidad = speed * Time.deltaTime;
+            _rb.velocity = transform.up * speed * -direccion.magnitude;
+        }
+        else {
+            
+            float distancia = Vector3.Distance(transform.position, _startMovPoint.position);
+            if (distancia < 1f)
+            {
+                current = 0;
+                //_rotAngle = Mathf.Atan2(_startMovPoint.position.x, _startMovPoint.position.) * Mathf.Rad2Deg - 90;
+            }
+            else
+            {
+                Vector3 direccion = (transform.position - _startMovPoint.position).normalized;
+                float velocidad = speed * Time.deltaTime;
+                _rb.velocity = transform.up * speed * direccion.magnitude;
+            }
+        }
+        
+    }
+}
