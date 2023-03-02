7feea55a9feb181958f8cbb13025fc5bfe880222
brokie636 <alejooo636@gmail.com>
2023-03-01 19:53:12 -0500

    Solución Taller 2 by aorozcooc

--- intro-basics-2023-1/Assets/Scripts/EnemyTankShooter.cs
+++ intro-basics-2023-1/Assets/Scripts/EnemyTankShooter.cs
@@ -4,39 +4,14 @@ using UnityEngine;
 
 public class EnemyTankShooter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    
-    /*[SerializeField]
-    private GameObject _projectilePrefab;
-    [SerializeField]
-    private Transform _shootPoint1;
-    [SerializeField]
-    private Transform _shootPoint2;
-
-    public float initialDelay=2.0f;
-    public float timeBetweenShots=1.0f;
-    void Start()
-    {
-         Invoke("Shoot", initialDelay);
-    }
-
-    // Update is called once per frame
-    void Shoot()
-    {
-        GameObject projectile1 = Instantiate(_projectilePrefab, _shootPoint1.position, _shootPoint1.rotation);
-        GameObject projectile2 = Instantiate(_projectilePrefab, _shootPoint2.position, _shootPoint2.rotation);
-        Invoke("Shoot",timeBetweenShots);
-    }
-    
-}*/
  [SerializeField]
     private GameObject projectilePrefab;
 
     [SerializeField]
     private Transform[] shootPoint;
 
-    public float timeRemaining = 10;
-    public float timeRemaining1 = 10;
+    public float timeRemaining = 1;
+    public float timeRemaining1 = 1;
 
     void Update()
     {
