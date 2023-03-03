d6489c2a3b0ba4358b26b233b4a7ea82f41ded94
José <jruedat@unal.edu.co>
2023-03-02 23:35:59 -0500

    Done :D

--- intro-basics-2023-1/Assets/Scripts/Bullet.cs
+++ intro-basics-2023-1/Assets/Scripts/Bullet.cs
@@ -1,36 +1,24 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+public class Bullet : MonoBehaviour{
 
-public class Bullet : MonoBehaviour
-{
-    [SerializeField]
-    private float _speed = 7;
+     [SerializeField]
+     private float _speed = 7;
     
-    private Rigidbody2D _rb;
+     private Rigidbody2D _rb;
 
-    private void Start()
-    {
-        _rb = GetComponent<Rigidbody2D>();
-        _rb.velocity = transform.up * _speed;
-    }
+     private void Start(){
+          _rb = GetComponent<Rigidbody2D>();
+          _rb.velocity = transform.up * _speed;
+     }
 
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        Debug.Log("Hit with " + other.collider.name);
-        DestroyProjectile();
-    }
+	private void OnTriggerEnter2D(Collider2D col){
+	     Debug.Log("Hit with " + col.name);
+          DestroyProjectile();
+     }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        Debug.Log("Hit with " + other.name);
-        DestroyProjectile();
-    }
+     private void DestroyProjectile(){
+          gameObject.SetActive(false);
+          Destroy(gameObject);
+     }
 
-    private void DestroyProjectile()
-    {
-        gameObject.SetActive(false);
-        Destroy(gameObject);
-    }
 }
--- intro-basics-2023-1/Assets/Scripts/PlayerTankShooter.cs
+++ intro-basics-2023-1/Assets/Scripts/PlayerTankShooter.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using IntroGames.Planets;
 using UnityEngine;
 
 public class PlayerTankShooter : MonoBehaviour
@@ -11,14 +8,14 @@ public class PlayerTankShooter : MonoBehaviour
     [SerializeField]
     private Transform _shootPoint;
     
-    void Update()
-    {
-        if (Input.GetButtonDown("Fire1"))
-        {
+    void Update(){
+        if (Input.GetButtonDown("Fire1")){
+
             //Shoot
             GameObject projectile = Instantiate(_projectilePrefab);
             projectile.transform.position = _shootPoint.position;
             projectile.transform.rotation = _shootPoint.rotation;
         }
     }
+
 }
