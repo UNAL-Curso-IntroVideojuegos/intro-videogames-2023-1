3ca959221c84fe3f80c6c22b81ab3d8ef6df21c3
Sergio <sebermudezg@unal.edu.co>
2023-03-03 00:19:00 -0500

    Taller2 done

---
+++ intro-basics-2023-1/Assets/Scripts/TriggerDetection.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDetection : MonoBehaviour
+{
+    [SerializeField] private GameObject barrera;
+    [SerializeField] private GameObject enemigo;
+    private BoxCollider2D bc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bc = GetComponent<BoxCollider2D>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        barrera.SetActive(false);
+        enemigo.SetActive(true);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
