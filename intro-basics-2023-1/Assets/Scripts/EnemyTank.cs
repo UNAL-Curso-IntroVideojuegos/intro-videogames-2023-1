9794b7ef7df9b15b83a90c91570fd6e7cad46f24
jtobonz <92930935+jtobonz@users.noreply.github.com>
2023-03-03 21:27:38 -0500

    EnemyBullet, Tank Color

--- intro-basics-2023-1/Assets/Scripts/EnemyTank.cs
+++ intro-basics-2023-1/Assets/Scripts/EnemyTank.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Diagnostics;
 using System.Security.Cryptography;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 public class EnemyTank : MonoBehaviour
 {
@@ -16,10 +19,16 @@ public class EnemyTank : MonoBehaviour
     [SerializeField]
     private Transform _player;
 
+    [Space(20)]
+    [SerializeField]
+    private GameObject _prefab;
+
 
     [Space(20)]
     [SerializeField]
     private Transform[] _cannon;
+    [SerializeField]
+    private Transform[] _sp; //ShootPoints
 
     [Space(20)]
     [SerializeField]
@@ -31,6 +40,8 @@ public class EnemyTank : MonoBehaviour
 
     private float dist;
 
+    private float _timer = 2;
+
 
     private void Start()
     {
@@ -38,16 +49,34 @@ public class EnemyTank : MonoBehaviour
         _self.SetActive(false);
     }
 
-    void Update()    
+    void Update()
     {
+        //Movimiento lateral
         dist = _initpos.position.y - _endpos.position.y;
-        transform.position = Vector3.Lerp(_initpos.position, _endpos.position, Mathf.PingPong(Time.time, dist)/dist);
+        transform.position = Vector3.Lerp(_initpos.position, _endpos.position, Mathf.PingPong(Time.time, dist) / dist);
 
+        //Cannons
         foreach (Transform cannon in _cannon)
         {
             Vector3 aimVector = (_player.position - cannon.position).normalized;
             float angle = Mathf.Atan2(aimVector.y, aimVector.x) * Mathf.Rad2Deg + 90;
             cannon.rotation = Quaternion.Euler(0, 0, angle);
+        }
+        
+        if (_timer > 0)
+        {
+            _timer -= Time.deltaTime;
+        }
+        else
+        {
+            foreach (Transform sp in _sp)
+            {
+                GameObject projectile = Instantiate(_prefab);
+                projectile.transform.position = sp.position;
+                projectile.transform.rotation = sp.rotation;
+            }
+            _timer = 2;
+            Debug.Log("Time has run out!");
         }
     }
 }
