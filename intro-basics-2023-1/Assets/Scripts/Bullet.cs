095d8356546b3f841ce181e0ba60a59cef7d1542
Alejo Alvarez <juaalvarezme@gmail.com>
2023-03-04 11:00:43 -0500

    Session 4

--- intro-basics-2023-1/Assets/Scripts/Bullet.cs
+++ intro-basics-2023-1/Assets/Scripts/Bullet.cs
@@ -7,27 +7,57 @@ public class Bullet : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 7;
+    [SerializeField]
+    private LayerMask _collisionMaks;
+    
+    [SerializeField]
+    private float _lifeTime = 3; //sec
     
     private Rigidbody2D _rb;
+    private float _destructionTime = 0;
 
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-        _rb.velocity = transform.up * _speed;
+        _destructionTime = Time.time + _lifeTime;
     }
 
-    private void OnCollisionEnter2D(Collision2D other)
+    private void Update()
     {
-        Debug.Log("Hit with " + other.collider.name);
-        DestroyProjectile();
+        if (Time.time > _destructionTime )
+        {
+            DestroyProjectile();
+        }
     }
-
-    private void OnTriggerEnter2D(Collider2D other)
+    
+    private void FixedUpdate()
     {
-        Debug.Log("Hit with " + other.name);
-        DestroyProjectile();
-    }
+        Vector2 dir = transform.up;
+        Vector2 movement = dir * _speed * Time.fixedDeltaTime;
+        Vector2 pos = _rb.position + movement;
 
+        CheckCollision(movement);
+        
+        _rb.MovePosition(pos);
+    }
+    
+    private void CheckCollision(Vector2 movement)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(_rb.position, transform.up, movement.magnitude, _collisionMaks);
+        
+        // If it hits something...
+        if (hit.collider != null)
+        {
+            if (hit.transform.TryGetComponent(out IDamageable targetHit))
+            {
+                targetHit.TakeHit();
+            }
+            
+            Debug.Log("Hit with " + hit.collider.name);
+            DestroyProjectile();
+        }
+    }
+    
     private void DestroyProjectile()
     {
         gameObject.SetActive(false);
---
+++ intro-basics-2023-1/Assets/Scripts/IDamageable.cs
@@ -0,0 +1,9 @@
+
+
+public interface IDamageable
+{
+    public int TotalHealthPoints { get; }
+    public int HealthPoints { get; }
+    
+    public void TakeHit();
+}
