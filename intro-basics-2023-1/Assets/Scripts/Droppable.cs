2deadc9dc2f8645f459caee0624d7fb83ea4ee5e
Alejoab <aalvarezbo@unal.edu.co>
2023-03-05 02:30:32 -0500

    Añade caja looteable

---
+++ intro-basics-2023-1/Assets/Scripts/Droppable.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Droppable : MonoBehaviour, IDamageable
+{
+    [field:SerializeField]
+    public int TotalHealthPoints {  get; private set; }
+    public int HealthPoints { get; private set; }
+
+    [SerializeField]
+    private float LootProbability;
+
+    [SerializeField]
+    private GameObject[] LootList;
+
+
+    void Start()
+    {
+        HealthPoints = TotalHealthPoints;
+    }
+
+    public void TakeHit()
+    {
+        if (HealthPoints <= 0)
+        {
+            return;
+        }
+        HealthPoints--;
+        if (HealthPoints <= 0)
+        {
+            gameObject.SetActive(false);
+
+            // Soltar Loot
+            float r = Random.Range(0.0f, 1.0f);
+            Debug.Log("Random number "+r);
+            if (r <= LootProbability)
+            {
+                GameObject loot = Instantiate(LootList[Random.Range(0, LootList.Length)]);
+                loot.transform.position = (Vector2) transform.position + Random.insideUnitCircle * 2;
+            }
+        }
+    }
+}
