306498d12adab6f83b7f59c42ce6262f85f44e0a
Diegovalen47 <osoriovalentin5@gmail.com>
2023-03-07 15:37:29 -0500

    Loot element rotating and dissapears when player touches it

---
+++ intro-basics-2023-1/Assets/Scripts/Lootable.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lootable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        gameObject.SetActive(false);
+    }
+}
