01efc148db9c87dc685e9e41197245ef7bd98811
Leimar Ocampo <bocampo@unal.edu.co>
2023-03-07 23:50:30 -0500

    Solución Taller 3 by bocampo

---
+++ intro-basics-2023-1/Assets/Scripts/Looteable.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Looteable : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 100;
+    [SerializeField]
+    private Transform centro;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(centro.position, Vector3.forward, speed * Time.deltaTime);
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Loot grabbed by Player");
+        gameObject.SetActive(false);
+    }
+}
