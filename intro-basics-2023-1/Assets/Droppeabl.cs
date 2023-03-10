81f4e37583fd1d289443eff29d09295c07728822
Tomás Rodríguez <torodriguezt@unal.edu.co>
2023-03-09 21:12:54 -0500

    Solución Taller 3 by torodriguezt
    Solución Taller 3 by torodriguezt

---
+++ intro-basics-2023-1/Assets/Droppeabl.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Droppabl : MonoBehaviour, IDamageable
+{
+    [field:SerializeField]
+    public int TotalHealthPoints { get; private set; }
+    public int HealthPoints { get; private set; }
+    public GameObject[] objectsToDrop; // array of objects to drop
+    public float dropProbability; // probability of dropping an object
+    // Start is called before the first frame update
+    void Start()
+    {
+        HealthPoints = TotalHealthPoints;
+    }
+
+    public void TakeHit()
+    {
+        if(HealthPoints <= 0)
+            return;
+    
+        HealthPoints--;
+        if(HealthPoints <= 0)
+            gameObject.SetActive(false);
+            DropObject();
+    }
+    void DropObject()
+{
+    if (Random.Range(0f, 1f) <= dropProbability)
+    {
+        int randomIndex = Random.Range(0, objectsToDrop.Length);
+        GameObject droppedObject = Instantiate(objectsToDrop[randomIndex], transform.position, Quaternion.identity);
+    }
+    
+    // Update is called once per frame
+    /*void Update()
+    {
+        
+    }*/
+}
+}
---
+++ intro-basics-2023-1/Assets/Scripts/Loot.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loot : MonoBehaviour, IDamageable
+{
+    [field:SerializeField]
+    public int TotalHealthPoints { get; private set; }
+    public int HealthPoints { get; private set; }
+    public GameObject[] objectsToDrop; // array of objects to drop
+    public float dropProbability; // probability of dropping an object
+    public float radio = 0.3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HealthPoints = TotalHealthPoints;
+    }
+
+    public void TakeHit()
+    {
+        if(HealthPoints <= 0)
+            return;
+    
+        HealthPoints--;
+        if(HealthPoints <= 0){
+            if(Random.value < dropProbability){
+                DropObject();
+            }
+            gameObject.SetActive(false);
+        }
+    }
+    void DropObject()
+{
+
+        int randomIndex = Random.Range(0, objectsToDrop.Length);
+        Vector2 circulo = Random.insideUnitCircle;
+        Vector3 conversion = new Vector3(circulo.x, 0f, circulo.y);
+        conversion = conversion*radio;
+        Vector3 nuevo = transform.position + conversion;
+        Instantiate(objectsToDrop[randomIndex], nuevo, Quaternion.identity);
+
+    // Update is called once per frame
+    /*void Update()
+    {
+        
+    }*/
+}
+}
