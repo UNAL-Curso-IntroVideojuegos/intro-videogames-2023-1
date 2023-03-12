80c84aa9f4786965a163e2a900e2593c1153c60e
alejo9604 <juaalvarezme@gmail.com>
2023-03-12 15:27:17 -0500

    Session #5 - Animator and 3D

---
+++ intro-3d-2023-1/Assets/Scripts/WizardAnimator.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WizardAnimator : MonoBehaviour
+{
+    [SerializeField]
+    private Animator _anim;
+
+    private bool _isAttacking = false;
+
+    void Update()
+    {
+        //Logic
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _isAttacking = true;
+        }
+        
+        //Update animations
+        _anim.SetBool("IsAttacking1", _isAttacking);
+        _anim.SetBool("IsAttacking2", _isAttacking);
+        //_anim.SetFloat("Velocity", 0);
+
+        if (_isAttacking)
+        {
+            _anim.SetTrigger("Attack");
+            _isAttacking = false;
+        }
+    }
+}
