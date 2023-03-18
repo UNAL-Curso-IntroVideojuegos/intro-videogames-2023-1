434d7e18dd994661bb3bd9b5ae48fba514efa1a7
alejo9604 <juaalvarezme@gmail.com>
2023-03-18 10:00:00 -0500

    Added PlayerController

--- intro-3d-2023-1/Assets/Scripts/Player/PlayerController.cs
+++ intro-3d-2023-1/Assets/Scripts/Player/PlayerController.cs
@@ -5,13 +5,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Player")]
+    [SerializeField] private float _moveSpeed = 2.0f;
+    [SerializeField] private float _rotationSpeed = 15.0f;
+    
     private CharacterController _controller;
     private PlayerInput _input;
 
+    private void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        _input = GetComponent<PlayerInput>();
+    }
 
     private void Update()
     {
-        Vector2 moveDirection = _input.Move;
-        Vector3 pointToLook = _input.Look;
+        Move();
+
+    }
+
+    private void Move()
+    {
+        float targetSpeed = _moveSpeed;
+        if (_input.Move == Vector2.zero)
+        {
+            targetSpeed = 0.0f;
+        }
+
+        Vector3 targetMoveDirection = new Vector3(_input.Move.x, 0.0f, _input.Move.y).normalized;
+
+        _controller.Move(targetMoveDirection * targetSpeed * Time.deltaTime);
     }
 }
