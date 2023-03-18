d1a1d5bae0bda20ab337524118bd9a7c343fc6b6
alejo9604 <juaalvarezme@gmail.com>
2023-03-18 10:41:47 -0500

    Added Playe rotation

--- intro-3d-2023-1/Assets/Scripts/Player/PlayerController.cs
+++ intro-3d-2023-1/Assets/Scripts/Player/PlayerController.cs
@@ -8,6 +8,12 @@ public class PlayerController : MonoBehaviour
     [Header("Player")]
     [SerializeField] private float _moveSpeed = 2.0f;
     [SerializeField] private float _rotationSpeed = 15.0f;
+    [SerializeField] private float _gravity = -15;
+
+    [Header("Animator")]
+    [SerializeField] private Transform _body;
+
+    private float _verticalVelocity;
     
     private CharacterController _controller;
     private PlayerInput _input;
@@ -21,6 +27,7 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         Move();
+        LookAt();
 
     }
 
@@ -31,9 +38,17 @@ public class PlayerController : MonoBehaviour
         {
             targetSpeed = 0.0f;
         }
-
+ 
         Vector3 targetMoveDirection = new Vector3(_input.Move.x, 0.0f, _input.Move.y).normalized;
 
-        _controller.Move(targetMoveDirection * targetSpeed * Time.deltaTime);
+        _verticalVelocity += _gravity * Time.deltaTime;
+        
+        _controller.Move((targetMoveDirection * targetSpeed * Time.deltaTime) + 
+                         Vector3.up * _verticalVelocity * Time.deltaTime);
+    }
+
+    private void LookAt()
+    {
+        _body.rotation = Quaternion.Slerp( _body.rotation, Quaternion.LookRotation(_input.Look), _rotationSpeed * Time.deltaTime);
     }
 }
