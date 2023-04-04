ecf93b07d67adeb646a33d471126ac196cdc06fa
PabloRestrepo01 <parestrepoo@unal.edu.co>
2023-04-04 15:29:52 -0500

    Avance3

--- intro-3d-2023-1/Assets/Scripts/Enemy/FiniteStateMachine/Decisions/InDetectionZoneCheck.cs
+++ intro-3d-2023-1/Assets/Scripts/Enemy/FiniteStateMachine/Decisions/InDetectionZoneCheck.cs
@@ -6,10 +6,6 @@ public class InDetectionZoneCheck : StateDecision
     public override bool Check(FiniteStateMachine fms)
     {
         float distance = (fms.Target.position - fms.transform.position).magnitude;
-        if (distance <= fms.Config.DetectionRange)
-        {
-            return true;
-        }
-        return false;
+        return distance <= fms.Config.DetectionRange;
     }
 }
