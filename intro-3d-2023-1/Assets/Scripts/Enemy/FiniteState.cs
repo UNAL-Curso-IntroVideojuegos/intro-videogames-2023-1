b53ec38b234597242edee74e0be4365e6d32ef44
Matius420 <111587777+Matius420@users.noreply.github.com>
2023-04-08 20:13:52 -0500

    Add files via upload
    Finite State Machine (basado en instrucciones del taller)

---
+++ intro-3d-2023-1/Assets/Scripts/Enemy/FiniteState.cs
@@ -0,0 +1,23 @@
+
+public class PatrolState : State
+{
+    public override StateType Type => StateType.Patrol;
+
+    public PatrolState() : base("Patrol") { }
+    
+    protected override void OnEnterState(FiniteStateMachine fms)
+    {
+        fms.SetMovementSpeed(fms.Config.Speed);
+    }
+
+    protected override void OnUpdateState(FiniteStateMachine fms, float deltaTime)
+    {
+        if(fms.NavMeshController.IsAtDestination())
+            fms.NavMeshController.GoToNextWaypoint();
+    }
+
+    protected override void OnExitState(FiniteStateMachine fms)
+    {
+        fms.NavMeshController.StopAgent();
+    }
+}
